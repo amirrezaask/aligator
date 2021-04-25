bd175e1c582f1706c20419f2090876a0639b245b
amirrezaask <raskarpour@gmail.com>
2021-04-25 14:22:46 +0430

    basic json config

--- lua/index.lua
+++ lua/index.lua
@@ -1,8 +1,22 @@
 local M = {}
 local ngx = require('ngx')
+local json = require('json')
+
+function M.read_config(path)
+  local config_file = io.open(path, 'rb')
+  assert(config_file, 'cannot read config file')
+  local config = config_file:read('*a')
+  config_file:close()
+  return config
+end
 
 function M.index()
-  ngx.say(table.concat({ 'amirreza', 'parsa', 'hesam' }, '\n'))
+  local config = json.decode(M.read_config('/etc/agg/config.json'))
+  local output = {}
+  for k, v in pairs(config) do
+    table.insert(output, k .. ' ' .. v)
+  end
+  ngx.say(table.concat(output, '\n'))
 end
 
 return M
