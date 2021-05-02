41ade12937c29d9136e160453acde246b0116100
amirrezaask <raskarpour@gmail.com>
2021-05-02 22:47:57 +0430

    WIP: config syntax

--- lua/aggregator.lua
+++ lua/aggregator.lua
@@ -1,10 +1,43 @@
 local M = {}
 local ngx = require('ngx')
 local json = require('lunajson')
-local config = require('config')
+local Config = require('config')
+local Group = Config.Group
+local API = Config.API
 
-function M.request_print()
-  ngx.say(json.encode({ name = 'amirreza' }))
+local config = Group:new({
+  product = Group:new({
+    get_id = API:new({
+      backends = {
+        {
+          name = 'productobj',
+          host = 'localhost:8080',
+          uri = 'products/' .. 'productid',
+          method = 'get',
+          headers = {},
+          transformer = 'json.decode',
+          save_to_state = {
+            'category_id',
+          },
+        },
+        {
+          name = 'categoryobj',
+          host = 'localhost:8080',
+          uri = 'category/' .. ngx.ctx.State.category_id,
+          method = 'get',
+          headers = {},
+          transformer = 'json.decode',
+        },
+      },
+      returns = function() end,
+    })
+  })
+})
+
+function M.mux()
+  local linearized = config:linearize('/')
+  local resp = linearized[ngx.var.request_uri]:call()
+  ngx.say(json.encode(resp))
 end
 
 return M
--- lua/config.lua
+++ lua/config.lua
@@ -1,32 +1,70 @@
-local json = require('lunajson')
+local ngx = require('ngx')
+
+local function split(inputstr, sep)
+  if sep == nil then
+    sep = "%s"
+  end
+  local t={}
+  for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+    table.insert(t, str)
+  end
+  return t
+end
 
 local Types = {
   Group = 'group',
   API = 'api',
 }
 
-local function group(apis)
-  return { ['type'] = Types.Group, apis = apis }
+local Group = {}
+Group.__index = Group
+Group.__call = Group.new
+
+-- @returns Group
+function Group:new(opts)
+  opts = opts or {}
+  opts['type'] = Types.Group
+  return setmetatable(opts, Group)
 end
 
-local function api(opts)
+function Group:linearize(base)
+  local new_t = {}
+  for route, obj in pairs(self) do
+    if route == 'type' then goto continue end
+    if obj['type'] == Types.Group then
+      local linearized = obj:linearize(base .. route)
+      for r, obj in pairs(linearized) do
+        new_t[r] = obj
+      end
+    elseif obj['type'] == Types.API then
+      new_t[base.. '/' .. route] = obj
+    end
+    ::continue::
+  end
+  return new_t
+end
+
+local API = {}
+API.__index = API
+API.__call = API.new
+
+-- @returns API
+function API:new(opts)
   opts = opts or {}
+  opts['type'] = Types.API
   assert(opts.host, 'need a host for API')
   assert(opts.method, 'need a method for API')
   assert(opts.transformer, 'need a transformer for API')
-  return opts
+  return setmetatable(opts, API)
 end
 
-return {
-  product = group {
-    api {
-      host = 'localhost:8080',
-      uri = 'products/' .. 'productid',
-      method = 'get',
-      headers = {},
-      transformer = function(res)
-        return json.decode(res.body)
-      end,
-    },
-  },
-}
+-- @returns API
+function Group:get_api_for_uri()
+  local uri = ngx.var.request_uri
+  uri = split(uri, '/')
+  for _, part in pairs(uri) do
+    ngx.log(ngx.ERR, part)
+  end
+end
+
+return { Group = Group, API = API }
